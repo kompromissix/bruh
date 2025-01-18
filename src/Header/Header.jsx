3137bfb39fb04729cda6c0ab002cf669ffb5deef
kompromiss <artemprodos@gmail.com>
2025-01-18 16:19:03 +0200

    kjh

---
+++ src/Header/Header.jsx
@@ -0,0 +1,28 @@
+import './Header.css'
+import logo from './Header-assets/logo.png'
+import burg from './Header-assets/ic_menu_48px.png'
+import illust from './Header-assets/illustration.png'
+
+export default function Header(){
+    return(
+        <>
+            <header>
+                <nav>
+                    <img src={logo} alt="" />
+                    <img src={burg} alt="" />
+                </nav>
+                <div>
+                    <div>
+                        <h1>New Automation
+                        Tool for Your Home</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus tristique vulputate ultrices ut mauris tellus at. Posuere sollicitudin odio tellus elit.</p>
+                        <button type="button">See Our Project</button>
+                    </div>
+                </div>
+                <div className='absolut_img'>
+                    <img src={illust} alt="" />
+                </div>
+            </header>
+        </>
+    )
+}
